96a81145d92736111e86d88263d801013c685888
ElectonAmpere <82173777+ElectronAmpere@users.noreply.github.com>
2025-08-15 14:44:48 +0530

    Refactored generatefile to a source file for size reduction

--- src/generate_manual.c
+++ src/generate_manual.c
@@ -48,20 +48,3 @@ GenerateManualErrorType_t generateManual(char *buffer, size_t bufferSize,
 
   return GMERROR_NULL; // Success indicator as per the original
 }
-
-GenerateManualErrorType_t generateFile(const char *fileName, char *buffer,
-                                       size_t bufferSize) {
-
-  // Write buffer to file
-  FILE *file = fopen(fileName, "w");
-  if (!file) {
-    return GMERROR_FILE_OPEN_FAILED;
-  }
-  size_t bytesWritten = fwrite(buffer, 1, bufferSize, file);
-  if (bytesWritten != (size_t)bufferSize) {
-    fclose(file);
-    return GMERROR_FILE_WRITE_FAILED;
-  }
-  fclose(file);
-  return GMERROR_NULL;
-}
